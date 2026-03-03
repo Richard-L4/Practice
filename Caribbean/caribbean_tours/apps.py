773c0071bbab5155a42968de02cb5208fe1f9df0
Richard-L4 <rwells945@gmail.com>
2026-03-03 14:06:15 +0000

    initial Django setup

---
+++ Caribbean/caribbean_tours/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CaribbeanToursConfig(AppConfig):
+    name = 'caribbean_tours'
---
+++ Caribbean/env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault(
+    "SECRET_KEY",
+    'django-insecure-o#5p!%0815=upsgghpewo6_4c%5+-8u^qx=jirhggm!9&t=qef'
+)
+
+os.environ.setdefault('DEVELOPMENT', '1')
